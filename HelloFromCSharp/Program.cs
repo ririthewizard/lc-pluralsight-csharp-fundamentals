c2945b999711001be956b27add9c04e470236a87
ririthewizard <ririwilliams.rw@gmail.com>
2025-06-10 00:00:09 -0500

    First night down, just going through the fundamentals. This part should be a breeze, mostly learning specific syntax it seems like

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,1 +1,11 @@
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello everybody!");
+
+
+Console.WriteLine("Please enter your name: ");
+
+string name = Console.ReadLine();
+
+if (name.Length > 0 ) {
+    Console.WriteLine("Hello " + name + " it is nice to meet you!");
+}
