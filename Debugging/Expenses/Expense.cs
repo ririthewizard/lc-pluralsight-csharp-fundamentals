00489924a692f7201e13b963c5e55e34fee518af
ririthewizard <ririwilliams.rw@gmail.com>
2025-06-23 22:43:44 -0500

    Okay on to debugging. Trying to get DAP to work in NVIM, but about to just use VSCode

---
+++ Debugging/Expenses/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.Debugging.ConsoleApp.Expenses
+{
+    public class Expense
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public DateOnly Date { get; set; }
+        public double Amount { get; set; }
+    }
+}
